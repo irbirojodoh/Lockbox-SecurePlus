9f0c0df9b42f6147a3287a80c0d8320b3a51366b
Daffafahrizi <87596554+daffafahrizi@users.noreply.github.com>
2023-12-09 18:22:48 +0700

    sudah bisa semua, buzzer belomnyala kalo salah

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <math.h>
+#include <Keypad.h>
+#include <ESP32Servo.h>
+
 
 //********************* SETUP ESPNOW*****************************************//
 // REPLACE WITH YOUR RECEIVER MAC Address
@@ -72,11 +75,11 @@ void motionDetectionTask(void *pvParameters) {
     float ay_f = static_cast<float>(ay);
     float az_f = static_cast<float>(az);
 
-    Serial.printf("ax: %f, ay: %f, az: %f\n", ax_f / 16384.0f - 1, ay_f / 16384.0f, az_f / 16384.0f);
+    // Serial.printf("ax: %f, ay: %f, az: %f\n", ax_f / 16384.0f - 1, ay_f / 16384.0f, az_f / 16384.0f);
 
     // Calculate magnitude
     float motionMag = sqrt(pow(fabs(ax_f) / 16384.0f - 1, 2) + pow(fabs(ay_f) / 16384.0f, 2) + pow(fabs(az_f) / 16384.0f, 2));
-    Serial.println(motionMag);
+    // Serial.println(motionMag);
 
     // Check if motion is detected
     if (motionMag > motionThresholdHigh) {
@@ -106,50 +109,101 @@ void motionDetectionTask(void *pvParameters) {
 
 //********************************SETUP BUZZER************************************************************///
 
+//********************************SETUP KEYPAD************************************************************//
+
+const byte ROWS = 4;  // Jumlah baris pada keypad
+const byte COLS = 4;  // Jumlah kolom pada keypad
+
+// Definisikan layout keypad
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+byte rowPins[COLS] = { 27, 23, 32, 12 };
+byte colPins[ROWS] = { 33, 25, 26, 14 };
+
+Keypad keypad = Keypad(makeKeymap(keys), colPins, rowPins, COLS, ROWS);
+
+// Pin untuk mengendalikan servo
+const int servoPin = 3;  // Ganti dengan pin yang terhubung ke servo
+
+Servo lockServo;  // Objek untuk mengendalikan servo
+
+// Variabel untuk menyimpan PIN yang benar
+const char correctPIN[] = "1234";  // Ganti dengan PIN yang diinginkan
+
+char enteredPIN[5];  // Variabel untuk menyimpan PIN yang dimasukkan
+int pinIndex = 0;    // Variabel untuk melacak indeks PIN yang dimasukkan
+
+TaskHandle_t keypadTaskHandle;
+
+//***************KEYPAD SETUP*****************************************************//
+void checkPINTask(void * parameter) {
+  Serial.println("Silahkan Masukkan PIN");
+  for (;;) {
+    vTaskDelay(100); // Menunggu 100ms sebelum membaca keypad lagi
+    
+    char key = keypad.getKey();
+  
+    if (key != NO_KEY) {
+      Serial.println(key); // Menampilkan tombol yang ditekan
+      
+      // Memeriksa apakah tombol yang ditekan adalah digit (0-9)
+      if (isdigit(key)) {
+        enteredPIN[pinIndex++] = key; // Menyimpan karakter PIN yang dimasukkan
+        Serial.print("PIN yang dimasukkan: ");
+        Serial.println(enteredPIN);
+        
+        // Jika sudah dimasukkan 4 karakter (PIN), verifikasi PIN
+        if (pinIndex == 4) {
+          checkPIN();
+        }
+      } else if (key == 'C') {
+        returnServoToInitialPosition();
+      }
+    }
+  }
+}
+
+void checkPIN() {
+  enteredPIN[pinIndex] = '\0'; // Menambahkan null terminator ke akhir PIN
+  
+  // Verifikasi PIN
+  if (strcmp(enteredPIN, correctPIN) == 0) { // Membandingkan PIN yang dimasukkan dengan PIN yang benar
+    Serial.println("\nPIN Benar! Membuka Brankas");
+    //mengirim pesan ke esp cam
+    strcpy(myData.a, "/photo");
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    lockServo.write(90); // Posisi untuk membuka brankas (sesuaikan dengan kebutuhan)
+  } else {
+    Serial.println("\nPIN Salah! Brankas Tetap Tertutup");
+    digitalWrite(buzzerPin, HIGH);
+          //mengirim pesan ke esp cam
+      strcpy(myData.a, "/photo");
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    
+  }
+  
+  // Reset PIN yang dimasukkan
+  pinIndex = 0;
+  memset(enteredPIN, 0, sizeof(enteredPIN));
+}
+
+void returnServoToInitialPosition() {
+  Serial.println("\nKembali ke posisi awal / Menutup Brankas");
+  lockServo.write(0); // Posisi untuk menutup brankas (sesuaikan dengan kebutuhan)
+  delay(2000); // Waktu untuk menutup brankas (sesuaikan dengan kebutuhan)
+}
+
+//*********************************************SETUP KEYPAD**********************************//
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting...");
-  pinMode(buzzerPin, OUTPUT);
-  digitalWrite(buzzerPin, HIGH);
-  pinMode(solenoidPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-  digitalWrite(solenoidPin, HIGH);
-  // servo.attach(14);
-
-  // Initialize MPU6050
-  Wire.begin();
-  initMPU6050();
-
-  if (mpu.testConnection()) {
-    Serial.println("MPU6050 connection successful");
-  } else {
-    Serial.println("MPU6050 connection failed");
-    while (1)
-      ;
-  }
-
-  xTaskCreatePinnedToCore(
-    solenoidTask,
-    "SolenoidTask",
-    1000,
-    NULL,
-    1,
-    NULL,
-    1);
-
-  xTaskCreatePinnedToCore(
-    motionDetectionTask,
-    "MotionDetectionTask",
-    10000,
-    NULL,
-    2,
-    NULL,
-    1);
-
-  // Init Serial Monitor
-
-  // Set device as a Wi-Fi Station
+    // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
@@ -172,8 +226,60 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, HIGH);
+  pinMode(solenoidPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  digitalWrite(solenoidPin, HIGH);
+  // servo.attach(14);
+
+  // Initialize MPU6050
+  initMPU6050();
+
+  if (mpu.testConnection()) {
+    Serial.println("MPU6050 connection successful");
+  } else {
+    Serial.println("MPU6050 connection failed");
+    while (1)
+      ;
+  }
+
+  // xTaskCreatePinnedToCore(
+  //   solenoidTask,
+  //   "SolenoidTask",
+  //   1000,
+  //   NULL,
+  //   1,
+  //   NULL,
+  //   1);
+
+  xTaskCreatePinnedToCore(
+    motionDetectionTask,
+    "MotionDetectionTask",
+    10000,
+    NULL,
+    1,
+    NULL,
+    0);
+
+  // Init Serial Monitor
+    lockServo.attach(servoPin); // Menghubungkan servo ke pin yang ditentukan
+  lockServo.write(0); // Posisi awal servo (sesuaikan dengan kebutuhan)
+  
+  Serial.println("Sistem Brankas");
+  
+  xTaskCreatePinnedToCore(
+    checkPINTask,
+    "CheckPIN",
+    10000,
+    NULL,
+    2,
+    &keypadTaskHandle,
+    1
+  );
+
+
 }
 
 void loop() {
-
 }
